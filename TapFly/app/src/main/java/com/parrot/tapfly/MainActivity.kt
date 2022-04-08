4877936efe0541135d49823293e4deb40a7fe921
daichow <daiansask@gmail.com>
2022-04-08 11:05:01 -0400

    started from the base app again in tapfly

---
+++ TapFly/app/src/main/java/com/parrot/tapfly/MainActivity.kt
@@ -0,0 +1,285 @@
+package com.parrot.tapfly
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.parrot.drone.groundsdk.GroundSdk
+import com.parrot.drone.groundsdk.ManagedGroundSdk
+import com.parrot.drone.groundsdk.Ref
+import com.parrot.drone.groundsdk.device.DeviceState
+import com.parrot.drone.groundsdk.device.Drone
+import com.parrot.drone.groundsdk.device.RemoteControl
+import com.parrot.drone.groundsdk.device.instrument.BatteryInfo
+import com.parrot.drone.groundsdk.device.peripheral.StreamServer
+import com.parrot.drone.groundsdk.device.peripheral.stream.CameraLive
+import com.parrot.drone.groundsdk.device.pilotingitf.Activable
+import com.parrot.drone.groundsdk.device.pilotingitf.ManualCopterPilotingItf
+import com.parrot.drone.groundsdk.facility.AutoConnection
+import com.parrot.drone.groundsdk.stream.GsdkStreamView
+
+class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var groundSdk: GroundSdk
+
+    private var drone: Drone? = null
+    private var droneStateRef: Ref<DeviceState>? = null
+    private var droneBatteryInfoRef: Ref<BatteryInfo>? = null
+    private var pilotingItfRef: Ref<ManualCopterPilotingItf>? = null
+    private var streamServerRef: Ref<StreamServer>? = null
+    private var liveStreamRef: Ref<CameraLive>? = null
+    private var liveStream: CameraLive? = null
+
+    private var rc: RemoteControl? = null
+    private var rcStateRef: Ref<DeviceState>? = null
+    private var rcBatteryInfoRef: Ref<BatteryInfo>? = null
+
+
+    private lateinit var streamView: GsdkStreamView
+    private lateinit var droneStateTxt: TextView
+    private lateinit var droneBatteryTxt: TextView
+    private lateinit var rcStateTxt: TextView
+    private lateinit var rcBatteryTxt: TextView
+    private lateinit var takeOffLandBt: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        streamView = findViewById(R.id.stream_view)
+        droneStateTxt = findViewById(R.id.droneStateTxt)
+        droneBatteryTxt = findViewById(R.id.droneBatteryTxt)
+        rcStateTxt = findViewById(R.id.rcStateTxt)
+        rcBatteryTxt = findViewById(R.id.rcBatteryTxt)
+        takeOffLandBt = findViewById(R.id.takeOffLandBt)
+        takeOffLandBt.setOnClickListener {onTakeOffLandClick()}
+
+        droneStateTxt.text = DeviceState.ConnectionState.DISCONNECTED.toString()
+        rcStateTxt.text = DeviceState.ConnectionState.DISCONNECTED.toString()
+
+        groundSdk = ManagedGroundSdk.obtainSession(this)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        groundSdk.getFacility(AutoConnection::class.java) {
+
+            it?.let{
+                if (it.status != AutoConnection.Status.STARTED) {
+                    it.start()
+                }
+
+                if (drone?.uid != it.drone?.uid) {
+                    if(drone != null) {
+                        stopDroneMonitors()
+
+                        resetDroneUi()
+                    }
+
+                    drone = it.drone
+                    if(drone != null) {
+                        startDroneMonitors()
+                    }
+                }
+
+                if (rc?.uid  != it.remoteControl?.uid) {
+                    if(rc != null) {
+                        stopRcMonitors()
+
+                        resetRcUi()
+                    }
+
+                    rc = it.remoteControl
+                    if(rc != null) {
+                        startRcMonitors()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun resetDroneUi() {
+        droneStateTxt.text = DeviceState.ConnectionState.DISCONNECTED.toString()
+        droneBatteryTxt.text = ""
+        takeOffLandBt.isEnabled = false
+        streamView.setStream(null)
+    }
+
+    private fun startDroneMonitors() {
+        monitorDroneState()
+
+        monitorDroneBatteryLevel()
+
+        monitorPilotingInterface()
+
+        startVideoStream()
+    }
+
+
+    private fun stopDroneMonitors() {
+        droneStateRef?.close()
+        droneStateRef = null
+
+        droneBatteryInfoRef?.close()
+        droneBatteryInfoRef = null
+
+        pilotingItfRef?.close()
+        pilotingItfRef = null
+
+        liveStreamRef?.close()
+        liveStreamRef = null
+
+        streamServerRef?.close()
+        streamServerRef = null
+
+        liveStream = null
+    }
+
+
+    private fun startVideoStream() {
+        streamServerRef = drone?.getPeripheral(StreamServer::class.java) { streamServer ->
+            if (streamServer != null) {
+                if(!streamServer.streamingEnabled()) {
+                    streamServer.enableStreaming(true)
+                }
+
+                if (liveStreamRef == null) {
+                    liveStreamRef = streamServer.live { liveStream ->
+
+                        if (liveStream != null) {
+                            if (this.liveStream == null) {
+
+                                streamView.setStream(liveStream)
+                            }
+
+                            if (liveStream.playState() != CameraLive.PlayState.PLAYING) {
+                                liveStream.play()
+                            }
+                        } else {
+                            streamView.setStream(null)
+                        }
+                        this.liveStream = liveStream
+                    }
+                }
+            } else {
+                liveStreamRef?.close()
+                liveStreamRef = null
+                streamView.setStream(null)
+            }
+        }
+    }
+
+
+    private fun monitorDroneState() {
+        droneStateRef = drone?.getState {
+
+            it?.let {
+                droneStateTxt.text = it.connectionState.toString()
+            }
+        }
+    }
+
+    private fun monitorDroneBatteryLevel() {
+        droneBatteryInfoRef = drone?.getInstrument(BatteryInfo::class.java) {
+
+            it?.let {
+                droneBatteryTxt.text = getString(R.string.percentage, it.batteryLevel)
+            }
+        }
+    }
+
+    private fun monitorPilotingInterface() {
+        pilotingItfRef = drone?.getPilotingItf(ManualCopterPilotingItf::class.java) {
+            if (it == null) {
+                takeOffLandBt.isEnabled = false
+            } else {
+                managePilotingItfState(it)
+            }
+        }
+    }
+
+    private fun managePilotingItfState(itf: ManualCopterPilotingItf) {
+        when(itf.state) {
+            Activable.State.UNAVAILABLE -> {
+                takeOffLandBt.isEnabled = false
+            }
+
+            Activable.State.IDLE -> {
+                takeOffLandBt.isEnabled = false
+
+                itf.activate()
+            }
+
+            Activable.State.ACTIVE -> {
+
+                when {
+                    itf.canTakeOff() -> {
+                        takeOffLandBt.isEnabled = true
+                        takeOffLandBt.text = getString(R.string.take_off)
+                    }
+                    itf.canLand() -> {
+                        takeOffLandBt.isEnabled = true
+                        takeOffLandBt.text = getString(R.string.land)
+                    }
+                    else ->
+                        takeOffLandBt.isEnabled = false
+                }
+            }
+        }
+    }
+
+    private fun onTakeOffLandClick() {
+        pilotingItfRef?.get()?.let { itf ->
+            if (itf.canTakeOff()) {
+                itf.takeOff()
+            } else if (itf.canLand()) {
+                itf.land()
+            }
+        }
+    }
+
+
+    private fun resetRcUi() {
+        rcStateTxt.text = DeviceState.ConnectionState.DISCONNECTED.toString()
+        rcBatteryTxt.text = ""
+    }
+
+
+    private fun startRcMonitors() {
+        monitorRcState()
+
+        monitorRcBatteryLevel()
+    }
+
+
+    private fun stopRcMonitors() {
+
+        rcStateRef?.close()
+        rcStateRef = null
+
+        rcBatteryInfoRef?.close()
+        rcBatteryInfoRef = null
+    }
+
+
+    private fun monitorRcState() {
+        rcStateRef = rc?.getState {
+
+            it?.let {
+                rcStateTxt.text = it.connectionState.toString()
+            }
+        }
+    }
+
+
+    private fun monitorRcBatteryLevel() {
+        rcBatteryInfoRef = rc?.getInstrument(BatteryInfo::class.java) {
+
+            it?.let {
+                rcBatteryTxt.text = getString(R.string.percentage, it.batteryLevel)
+            }
+        }
+    }
+}
